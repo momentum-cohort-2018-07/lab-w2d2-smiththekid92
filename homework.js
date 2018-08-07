ffbe129c89ffa42825b2a2b1236ed4a6eafce92d
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-07 18:57:59 -0400

    Completed additional problems. Submitting assignment, to be edited/fixed tomorrow.

--- homework.js
+++ homework.js
@@ -41,17 +41,15 @@ function intersection(array1, array2) {
 // 4. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
 
-function minimum(numbers)
-var numbers = []
-var min;
+function minimum(numbers) {
+  var minimum;
 
-for (var i = 0; i < numbers.length; i++) {
-  if (typeof (min) === 'undefined') {
-    min = numbers[i];
+  for (var i = 0; i < numbers.length; i++) {
+    if (typeof (minimum) === 'undefined' || numbers[i] < minimum) {
+      minimum = numbers[i];
+    }
   }
 }
-
-console.log(min);
 
 // 5. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -77,21 +75,67 @@ console.log(min);
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
 
+// function selectionSort(numbers) {
+//   var numbersCopy = numbers.slice(0);
+//   var minimum = minimum(numbers)
+//     for (var i = 0; i < numbers.length; i++) {
+//       numbers.sort()
+//   }
+
+// }
+
 // 6. Create a function called "createUser" that takes a name and a Date object
 // and returns an object with the keys "name" and "dob" (date of birth) with
 // those values.
+
+function createUser(name, date) {
+  var person = {
+    name: name, 
+    dob: date
+  }
+  return person
+}
 
 // 7. Create a function called "calculateAge" that takes a user created from
 // createUser and a Date object considered the current date, and calculates the user's
 // age in years on that date. You can use your code from yesterday's homework.
 
+function calculateAge (createUser, anotherDate){
+  function howOld (birthDate, anotherDate) {
+    var ageAtDate = anotherDate.getTime() - birthDate.getTime()  
+    return (Math.floor(ageAtDate / 31557600000))
+    }
+} return calculateAge
+
 // 8. Create a function called "addAge" that takes a user created from createUser
 // and a Date object and adds a new key on the user object, "age", with the age
 // in years the user was on that date.
 
+function addAge (date){
+  var age = []
+
+  for (var i =0; i < date; i++){
+    if (age === date + new Date){
+      return addAge
+    }
+  }
+}
+
 // 9. Create a function called "createUsers" that takes two arrays of equal
 // length, the first being a list of names and the second being a list of dates of
 // birth, and returns a new array of objects created from those original arrays.
+
+function createUsers(array1, array2){
+  var createUser = 
+  names =  []
+  dates: [new Date]
+
+  for (i = 0; i < array1.length; i++){
+    if (createUser === array1 && array2){
+      return dates
+    }
+  }
+}
 
 // 10. Create a function called "averageAge" that takes an array of users and
 // a Date object and returns the average age in years of the users on that date.
